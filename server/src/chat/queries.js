95fe3e41292dd6fa3ac6159bca20f34c0068934c
Brian Nkhata <bnkhata@mra.mw>
2023-03-14 15:17:14 +0200

    house keeping

--- server/src/chat/queries.js
+++ server/src/chat/queries.js
@@ -1,5 +1,5 @@
 const getAllchats = "SELECT * FROM `tblchats` WHERE `deleted` = 0";
-const getUserChats = "SELECT * FROM `tblchats` WHERE (`from` = ? AND `to` = ?) OR (`from` = ? AND `to` = ?) ORDER BY `chat_id`";
+const getUserChats = "SELECT * FROM `tblchats` WHERE (`from` = ? AND `to` = ?) OR (`to` = ? AND `from` = ?) ORDER BY `chat_id`";
 const addChat = "INSERT INTO `tblchats` (`from`, `to`, `message`) VALUES (?, ?, ?)";
 const deleteMessage =  "UPDATE `tblchats` SET `deleted` = 1 WHERE `chat_id` = ?";
 const readMessage =  "UPDATE `tblchats` SET `read` = 1 WHERE `chat_id` = ?";
